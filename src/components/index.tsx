ef6d0e49541f422b71bd27f8dadab3aa19ecee00
Paul Padamadan <paul@zephony.com>
2024-05-26 23:50:45 +0530

    Add slider component

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -10,40 +10,49 @@ const SliderWidget = (): JSX.Element => {
 
     return <div className='slider-widget'>
         <div className='display-value'>
-            <div className='striped-circle'>
-                <svg
-                    className='progress-ring'
-                    width='100%'
-                    height='100%'
-                    viewBox='0 0 100 100'
+            <svg
+                className='progress-ring'
+                width='100%'
+                height='100%'
+                viewBox='0 0 100 100'
+            >
+                <circle
+                    stroke='#ccc'
+                    strokeWidth='.5'
+                    strokeLinecap='round'
+                    fill='transparent'
+                    r='49'
+                    cx='50'
+                    cy='50'
+                    strokeDasharray='7'
+                />
+                <circle
+                    stroke='white'
+                    strokeWidth='2'
+                    strokeLinecap='round'
+                    fill='rgba(204, 204, 204, 0.2)'
+                    r='40'
+                    cx='50'
+                    cy='50'
+                    pathLength='10'
+                    strokeDasharray={`${value} ${10 - value}`}
+                    stroke-dashoffset="2.5"
+                />
+                <circle
+                    fill='#6fdbe7'
+                    r='30'
+                    cx='50'
+                    cy='50'
+                />
+                <text x='50' y='50'
+                    textAnchor='middle'
+                    dominantBaseline='middle'
+                    fill='white'
+                    fontSize='20'
                 >
-                    <circle
-                        stroke='white'
-                        strokeWidth='2'
-                        strokeLinecap='round'
-                        fill='rgba(204, 204, 204, 0.2)'
-                        r='25'
-                        cx='50'
-                        cy='50'
-                        pathLength='10'
-                        strokeDasharray={`${value} ${10 - value}`}
-                    />
-                    <circle
-                        fill='#6fdbe7'
-                        r='15'
-                        cx='50'
-                        cy='50'
-                    />
-                    <text x='50' y='50'
-                        textAnchor='middle'
-                        dominantBaseline='middle'
-                        fill='white'
-                        fontSize='10'
-                    >
-                        {value}
-                    </text>
-                </svg>
-            </div>
+                    {value}
+                </text>
+            </svg>
         </div>
         <div className='range-slider'>
             <input
@@ -52,7 +61,7 @@ const SliderWidget = (): JSX.Element => {
                 min='0'
                 max='10'
                 value={value}
-                onChange={e => Number(e.target.value)}
+                onChange={e => setValue(Number(e.target.value))}
                 style={sliderInputStyle}
             />
         </div>
