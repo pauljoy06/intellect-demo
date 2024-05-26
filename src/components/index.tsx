ddd13e2e71cb14a9ad4c7b43c44a619c1a059e4c
Paul Padamadan <paul@zephony.com>
2024-05-26 16:17:54 +0530

    Add basic responsiveness for slider wideget

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -11,32 +11,19 @@ const SliderWidget = (): JSX.Element => {
     };
 
     return <div className='slider-widget'>
-        {false && <div className='circle-slide'>
-            <div className='value'>{value}</div>
-            <svg className='progress-ring' width='120' height='120'>
-                <circle
-                    className='progress-ring__circle'
-                    stroke='white'
-                    strokeWidth='4'
-                    fill='transparent'
-                    r='52'
-                    cx='60'
-                    cy='60'
-                    style={{
-                        strokeDasharray: `${(value / 10) * 327}, 327`,
-                    }}
-                />
-            </svg>
-        </div>}
-        <input
-            className='range-slider'
-            type='range'
-            min='0'
-            max='10'
-            value={value}
-            onChange={onThumbChange}
-            style={sliderInputStyle}
-        />
+        <div className='display-value'>
+        </div>
+        <div className='range-slider'>
+            <input
+                className='slider'
+                type='range'
+                min='0'
+                max='10'
+                value={value}
+                onChange={onThumbChange}
+                style={sliderInputStyle}
+            />
+        </div>
     </div>
 }
 
